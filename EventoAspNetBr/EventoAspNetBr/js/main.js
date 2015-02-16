d5e3cdd4ca968283f77c69c9dfd818e91cd40cb7
Waldyr Felix <waldyrfelix@gmail.com>
2015-02-16 13:06:52 -0200

    adicionados palestrantes

--- EventoAspNetBr/EventoAspNetBr/js/main.js
+++ EventoAspNetBr/EventoAspNetBr/js/main.js
@@ -15,9 +15,6 @@
             fn.MainSliderAlign();
             fn.Navigation();
             fn.Carousel();
-            fn.Slider();
-            fn.RegisterForm();
-            fn.SubscribeForm();
             fn.Apps();
         },
 
@@ -77,7 +74,6 @@
         },
 
 
-
         // Align Slider Horizontally
         MainSliderAlign: function () {
             var imageWidth, widthFix, container = $('.header-bg');
@@ -88,8 +84,6 @@
             }
             $(window).on("load resize", centerImage);
         },
-
-
 
         // Main FlexSlider
         MainSlider: function () {
@@ -108,31 +102,27 @@
         },
 
 
-
         // One Page Navigation
         Navigation: function () {
             $('#menu').onePageNav({
                 currentClass: 'current',
                 scrollSpeed: 500,
                 scrollOffset: 60,
-                scrollThreshold: 0.2,
-                easing: 'swing'
+                scrollThreshold: 0.2
             });
         },
-
-
 
         // Owl Carousel
         Carousel: function () {
             var owl = $("#carousel");
             owl.owlCarousel({
-                itemsCustom : [
+                itemsCustom: [
                     [1200, 4],
                     [970, 3],
                     [768, 2],
                     [360, 1]
                 ],
-                navigation : false
+                navigation: false
             });
 
             $(".next").click(function () {
@@ -144,119 +134,14 @@
             });
         },
 
-
-
-        // FlexSlider
-        Slider: function () {
-            $('.flexslider').flexslider({
-                noCSS: true,
-                touch: false,
-                directionNav: false,
-                animation: "fade"
-            });
-        },
-
-
-
-        // Registration Form
-        RegisterForm: function () {
-            $("#register-form").submit(function (e) {
-                e.preventDefault();
-                var name = $("#name").val(),
-                    email = $("#email").val(),
-                    telephone = $("#telephone").val(),
-                    ticket = $("#ticket").val(),
-                    dataString = 'name=' + name + '&email=' + email + '&telephone=' + telephone + '&ticket=' + ticket;
-                function isValidEmail(emailAddress) {
-                    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-                    return pattern.test(emailAddress);
-                }
-
-                if (isValidEmail(email) && (name.length > 1)) {
-                    $.ajax({
-                        type: "POST",
-                        url: "php/process.php",
-                        data: dataString,
-                        success: function () {
-                            $('#register-form .form-notification').fadeIn(500);
-                        }
-                    });
-                } else {
-                    if (!isValidEmail(email)) {
-                        $('input#email').addClass('not-valid');
-                    } else {
-                        $('input#email').removeClass('not-valid');
-                    }
-                    if (name.length === 0) {
-                        $('input#name').addClass('not-valid');
-                    } else {
-                        $('input#name').removeClass('not-valid');
-                    }
-                }
-                return false;
-            });
-        },
-
-
-        // Subscribe Form
-        SubscribeForm: function () {
-            $("#subscribe-form").submit(function (e) {
-                e.preventDefault();
-                var subscriber = $("#subscriber").val(),
-                    dataString = '&subscriber=' + subscriber;
-                function isValidEmail(emailAddress) {
-                    var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-                    return pattern.test(emailAddress);
-                }
-
-                if (isValidEmail(subscriber)) {
-                    $.ajax({
-                        type: "POST",
-                        url: "php/process.php",
-                        data: dataString,
-                        success: function () {
-                            $('#subscribe-form .form-notification').fadeIn(500);
-                        }
-                    });
-                } else {
-                    $('input#subscriber').addClass('not-valid');
-                }
-                return false;
-            });
-        },
-
-
         // Apps
         Apps: function () {
-
             // Accordion
             $('.accordion-group').accordion();
 
-            // Go Top
-            $('#gotop').click(function () {
-                $('html, body').scrollTo($('#header'), 300);
-            });
-
-            // Go Register
-            $('#goregister').click(function () {
-                $('html, body').scrollTo($('#register'), 300);
-            });
-
-            // Placeholder for MailChimp Form
-            $('#mce-EMAIL').attr('placeholder', 'Your email...');
-
             // Placeholder's for IE9
             $('input, textarea').placeholder();
-
-            // Responsive Video's
-            $(".video").fitVids();
-
-            // Fancy Select
-            $('#ticket').fancySelect();
-
         }
-
-
     };
 
     $(document).ready(function () {
